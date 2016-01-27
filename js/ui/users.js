48766b865fcf5f8dce6e7b5ef06c34fdc746005b
Lain-dono <lain.dono@gmail.com>
2016-01-27 17:44:54 +0300

    refactor ui/users

--- js/ui/users.js
+++ js/ui/users.js
@@ -1,9 +1,38 @@
 'use strict'
 
+require('./users.styl')
+
 var dom = require('../dom.js')
 var Panel = require('../panel.js')
 
-module.exports = function Users() {
+var tabs = require('./tabs.js')
+
+module.exports = {
+    template: require('raw!./users.html'),
+    components: {
+        'v-tabs': tabs.tabs,
+        'tab': tabs.tab,
+    },
+    props: {
+        online: Array,
+        friends: Array,
+        blacklist: Array,
+    },
+    data: function() {
+        return {
+            tab: 'online',
+            ratingBase: sprintf('%s//%s/stats/', location.protocol, game.network.host),
+        }
+    },
+    methods: {
+        clickByName: function(event, name) {
+            return game.chat.nameMenu(event, name);
+        },
+    },
+}
+
+/*
+function Users() {
     var lists = {};
 
     function renderList(content, list, empty) {
@@ -95,3 +124,4 @@ module.exports = function Users() {
         }
     };
 }
+*/
